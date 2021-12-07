af6cf15afbfc3b9d5e55c2a78105f7a6b009ce19
aliemre.ipek <aliemre.ipek@outlook.com>
2021-12-07 12:11:37 +0300

    12/7/2021

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/aliemreipek/bookings/pkg/config"
 	"github.com/aliemreipek/bookings/pkg/models"
 	"github.com/aliemreipek/bookings/pkg/render"
@@ -9,36 +10,77 @@ import (
 
 // Repo is the repository used by handlers
 var Repo *Repository
+
 // Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
 }
+
 // NewRepo creates a new repository
-func NewRepo(a *config.AppConfig)*Repository{
+func NewRepo(a *config.AppConfig) *Repository {
 	return &Repository{
-		App : a,
+		App: a,
 	}
 }
+
 // NewHandlers sets the repository for handlers
-func NewHandlers(r *Repository)  {
+func NewHandlers(r *Repository) {
 	Repo = r
 }
+
 // Home is the home page handler
-func (m *Repository)Home(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.page.tmpl" , &models.TemplateData{})
+	render.RenderTemplate(w, "home.page.tmpl", r, &models.TemplateData{})
 }
 
 // About is the about page handler
-func (m *Repository)About(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello again"
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
-	render.RenderTemplate(w, "about.page.tmpl" , &models.TemplateData{
+	render.RenderTemplate(w, "about.page.tmpl", r, &models.TemplateData{
 		StringMap: stringMap,
 	})
 
+}
+
+// Reservation renders the make a reservation page and displays form
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+
+	render.RenderTemplate(w, "make-reservation.page.tmpl", r, &models.TemplateData{})
+}
+
+// Generals renders the room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+
+	render.RenderTemplate(w, "generals.page.tmpl", r, &models.TemplateData{})
+}
+
+// Majors renders the majors page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+
+	render.RenderTemplate(w, "majors.page.tmpl", r, &models.TemplateData{})
+}
+
+// Availability renders the search availability page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+
+	render.RenderTemplate(w, "search-availability.page.tmpl", r, &models.TemplateData{})
+}
+
+// PostAvailability posts the search availability page
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+	w.Write([]byte(fmt.Sprintf("Start date is : %s, end date is : %s", start, end)))
+}
+
+// Contact renders the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+
+	render.RenderTemplate(w, "contact.page.tmpl", r, &models.TemplateData{})
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -5,73 +5,74 @@ import (
 	"fmt"
 	"github.com/aliemreipek/bookings/pkg/config"
 	"github.com/aliemreipek/bookings/pkg/models"
+	"github.com/justinas/nosurf"
 	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 )
+
 var functions = template.FuncMap{}
 
 var app *config.AppConfig
 
-
 // NewTemplates sets the config for the template package
-func NewTemplates(a *config.AppConfig){
+func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
 // AddDefaultData adds a default data when semding data to templates
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 //RenderTemplate renders templates using html/template
-func RenderTemplate(w http.ResponseWriter, tmpl string , td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, r *http.Request, td *models.TemplateData) {
 	// get the template cache from app config
 	var tc map[string]*template.Template
-	if app.UseCache{
+	if app.UseCache {
 		tc = app.TemplateCache
-	} else{
-		tc , _ =CreateTemplateCache()
+	} else {
+		tc, _ = CreateTemplateCache()
 	}
-	t , ok := tc[tmpl]
-	if !ok{
+	t, ok := tc[tmpl]
+	if !ok {
 		log.Fatal("Could not get template from template cache")
 	}
-	AddDefaultData(td)
+	td = AddDefaultData(td, r)
 	buf := new(bytes.Buffer)
-	_ = t.Execute(buf , td)
-	_ , err := buf.WriteTo(w)
+	_ = t.Execute(buf, td)
+	_, err := buf.WriteTo(w)
 	if err != nil {
-		fmt.Println("Error writing template to browser" , err)
+		fmt.Println("Error writing template to browser", err)
 	}
 }
 
 // CreateTemplateCache creates a template cache as a map
-func CreateTemplateCache()(map[string]*template.Template , error){
-	myCache:= map[string]*template.Template{
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
 	}
-	pages , err := filepath.Glob("./templates/*.page.tmpl")
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
-			return myCache , err
+			return myCache, err
 		}
-		for _ , page := range pages {
-			name := filepath.Base(page)
-			ts , err := template.New(name).Funcs(functions).ParseFiles(page)
-			if err != nil {
-				return myCache , err
-			}
-			matches , err := filepath.Glob("./templates/*.layout.tmpl")
-			if err != nil {
-				return myCache , err
-			}
-			if len(matches) > 0 {
-				ts , err = ts.ParseGlob("./templates/*.layout.tmpl")
-				if err != nil {
-					return myCache , err
-				}
-			}
-			myCache[name] = ts
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
 		}
-		return myCache, nil
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
 }
