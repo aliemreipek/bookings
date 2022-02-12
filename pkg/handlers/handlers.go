8a5dfb034a24cf6091098cf72d1e67a810eaca48
aliemre.ipek <aliemre.ipek@outlook.com>
2022-02-12 20:23:59 +0300

    created a handler that return jsın

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/aliemreipek/bookings/pkg/config"
 	"github.com/aliemreipek/bookings/pkg/models"
 	"github.com/aliemreipek/bookings/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -77,6 +79,26 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("Start date is : %s, end date is : %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      false,
+		Message: "Available!",
+	}
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
